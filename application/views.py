7fdbb244386534c9baef6097e6f9c15b78d5009f
malinastanciu <malina_stanciu@outlook.com>
2022-01-08 13:27:51 +0200

    Created projects page

--- application/views.py
+++ application/views.py
@@ -1,18 +1,21 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
 from application.functions import create_context
 
 
-# Create your views here.
 @login_required(login_url='login')
 def dashboard(request):
     context = create_context(request)
     return render(request, 'application/dashboard.html', context)
 
 
-# Create your views here.
 @login_required(login_url='login')
 def account(request):
     context = create_context(request)
     return render(request, 'application/account.html', context)
+
+
+@login_required(login_url='login')
+def projects(request):
+    context = create_context(request)
+    return render(request, 'application/projects.html', context)
