33242648d851b3e132e2cfbd933e7a1db9923bfa
malinastanciu <malina_stanciu@outlook.com>
2022-01-08 13:09:14 +0200

    Created new models

--- application/models.py
+++ application/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import Group, User
+
 
 # Create your models here.
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    registration_date = models.DateField(auto_now_add=True)
+    final_date = models.DateField()
+
+
+class PathProject(models.Model):
+    id = models.AutoField(primary_key=True)
+    path = models.CharField(max_length=100)
+
+
+class Topics(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    projectID = models.ForeignKey(Project, on_delete=models.CASCADE)
+    studentID = models.ForeignKey(User, on_delete=models.CASCADE)
+    path = models.ForeignKey(PathProject, on_delete=models.CASCADE)
