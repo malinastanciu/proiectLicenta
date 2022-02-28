37991c4d965f4f9ef61079c8c5d8c3fde2ee260a
malinastanciu <malina_stanciu@outlook.com>
2022-02-28 19:52:31 +0200

    Added new models

--- application/models.py
+++ application/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import Group, User
-from django.contrib.postgres.fields import ArrayField
 
 
 # Create your models here.
+class Disciplina(models.Model):
+    id = models.AutoField(primary_key=True)
+    nume = models.CharField(max_length=100)
+    profesor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 class Proiect(models.Model):
     id = models.AutoField(primary_key=True)
     nume = models.CharField(max_length=100)
     data_inregistare = models.DateField(auto_now_add=True)
     data_finalizare = models.DateField()
     profesor = models.ForeignKey(User, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE, null=True)
     cale = models.CharField(max_length=100, default=None)
 
 
-class Disciplina(models.Model):
-    id = models.AutoField(primary_key=True)
-    nume = models.CharField(max_length=100)
-    proiecte = models.ManyToManyField(Proiect)
-    profesor = models.ForeignKey(User, on_delete=models.CASCADE)
+class Student(models.Model):
+    pass
